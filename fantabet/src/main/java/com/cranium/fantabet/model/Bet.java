671084df14954151afdc8cc4f6618b33176c7fd4
Braveheart <andrea.pacifico.85@gmail.com>
2014-09-16 23:17:23 +0200

    model updated

--- fantabet/src/main/java/com/cranium/fantabet/model/Bet.java
+++ fantabet/src/main/java/com/cranium/fantabet/model/Bet.java
@@ -1,8 +1,27 @@
 package com.cranium.fantabet.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+
+import com.cranium.fantabet.util.Deref;
+import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Load;
 
 @Entity
 public class Bet {
 
+	@Id	@Getter		private Long id;
+	@Load			private Ref<Person> better;
+	@Load			private List<Ref<SingleBet>> singleBets = new ArrayList<Ref<SingleBet>>();
+	
+	public Bet(Person better) {
+		this.better = Ref.create(better);
+	}
+	
+	public Person getBetter() { return Deref.deref(better); }
+	public List<SingleBet> getBets() { return Deref.deref(singleBets); }
 }
--- fantabet/src/main/java/com/cranium/fantabet/model/Person.java
+++ fantabet/src/main/java/com/cranium/fantabet/model/Person.java
@@ -1,14 +1,19 @@
 package com.cranium.fantabet.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import com.cranium.fantabet.util.Deref;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Load;
 
 @Entity
 @NoArgsConstructor
@@ -19,6 +24,7 @@ public class Person {
 	@Getter			private String email;
 	@Getter 		private Date created;
 	@Getter			private Date lastLogin;
+	@Load			private List<Ref<Tournament>> tournaments = new ArrayList<Ref<Tournament>>();
 	
 	public Person(String email) {
 		this.email = email;
@@ -32,4 +38,6 @@ public class Person {
 	public static Key<Person> key(long id) {
 		return Key.create(Person.class, id);
 	}
+	
+	public List<Tournament> getTournaments(){ return Deref.deref(tournaments); }
 }
